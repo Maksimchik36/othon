5e9586b2d3e4b472e593b98302154b8d6144e368
Maksimchik36 <Peltek1985@gmail.com>
2024-07-27 22:25:01 +0300

    completed footer

---
+++ src/data/footerInternationalOfficesData.js
@@ -0,0 +1,10 @@
+export const footerInternationalOfficesData = [
+    {
+        company: "Sangineni Engineering and Technical Services (SEATS) –",
+        location: "Hyderabad, India",
+    },
+    {
+        company: "Othon Enginero Consultares –",
+        location: "Santander, Spain",
+    },
+]
